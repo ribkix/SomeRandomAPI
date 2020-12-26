b3b0b50c64cd2e635a41105aa27adb732e007fc8
ribkix <42357978+ribkix@users.noreply.github.com>
2020-12-26 23:30:47 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     url="https://bruhdev.com",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming language :: Python :: 3",
+        "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
