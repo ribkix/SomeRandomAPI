7e068dda0f63e6d8c07d46a860fb582194c45592
ribkix <42357978+ribkix@users.noreply.github.com>
2020-12-26 23:37:33 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md","r") as fh:
 
 setuptools.setup(
     name="SomeRandomAPI",
-    version="0.0.1",
+    version="0.0.2",
     author="BruhDev",
     author_email="mr.bruh.dev@gmail.com",
     description="A wrapper library for some-random-api.",
